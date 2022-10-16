bc834c68cedf9f45dd95b57055fd23682a7668ee
EdwardGold Edem Hogan <edge37@outlook.com>
2022-10-16 15:59:43 +0100

    0.0.0.0

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require("express")
 const { ExpressPeerServer } = require("peer")
 
 const port = process.env.port || 3001
+const host = "0.0.0.0"
 const app = express()
 
-const server = app.listen(port, () => console.log(`listening on port ${port}`))
+const server = app.listen(port, host, () => console.log(`listening on port ${host}/${port}`))
 
 app.use(ExpressPeerServer(server, {
     debug: true,
