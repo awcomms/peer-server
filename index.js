e7b60dc93db069a745acd75064d39dd591b6ad0f
EdwardGold Edem Hogan <edge37@outlook.com>
2022-10-16 15:11:10 +0100

    express

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-const { PeerServer } = require('peer');
+const express = require("express")
+const http = require("http")
+const path = require("path")
+const { ExpressPeerServer} = require("peer")
 
-PeerServer({ port: process.env.PORT || 3001});
-console.log('running')
+const port = process.env.port || 3001
+
+const app = express()
+const server = http.createServer(app)
+
+const peerServer = ExpressPeerServer(server, {
+    proxied: true,
+    debug: true,
+})
+
+app.use(peerServer)
+
+server.listen(port, () => `listening on port ${port}`)
